469670570855a0d09b4181228d8c9920dc1d87dd
slastushevskyi <slastushevskyi@gmail.com>
2023-02-03 23:38:58 +0100

    try catch added

--- javascript/javascript3/week2/index.js
+++ javascript/javascript3/week2/index.js
@@ -1,26 +1,30 @@
 const currency = document.querySelectorAll(".currency");
 const button = document.querySelector(".exchange");
-
 const api_url = `https://open.er-api.com/v6/latest/USD`;
+
 async function getDataFromApi() {
-  const response = await fetch(api_url);
-  const data = await response.json();
-  const currencies = data.rates;
-  function getCurrencies(obj) {
-    for (const key in obj) {
-      const value = obj[key];
-      currency[0].innerHTML += `<option value=${value}>${key}</option>`;
-      if (key === "DKK") {
-        currency[0].innerHTML += `<option selected="selected" value=${value}>${key}</option>`;
-      } else if (key === "EUR") {
-        currency[1].innerHTML += `<option selected="selected" value=${value}>${key}</option>`;
-      } else {
+  try {
+    const response = await fetch(api_url);
+    const data = await response.json();
+    const currencies = data.rates;
+    function getCurrencies(obj) {
+      for (const key in obj) {
+        const value = obj[key];
         currency[0].innerHTML += `<option value=${value}>${key}</option>`;
-        currency[1].innerHTML += `<option value=${value}>${key}</option>`;
+        if (key === "DKK") {
+          currency[0].innerHTML += `<option selected="selected" value=${value}>${key}</option>`;
+        } else if (key === "EUR") {
+          currency[1].innerHTML += `<option selected="selected" value=${value}>${key}</option>`;
+        } else {
+          currency[0].innerHTML += `<option value=${value}>${key}</option>`;
+          currency[1].innerHTML += `<option value=${value}>${key}</option>`;
+        }
       }
     }
+    getCurrencies(currencies);
+  } catch (error) {
+    console.log(error);
   }
-  getCurrencies(currencies);
 }
 getDataFromApi();
 
